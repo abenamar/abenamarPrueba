26b7d63bca5e886117aeabfb29ffc86fe91cead2
i62chsaf <i62chsaf@uco.es>
2017-04-19 08:47:56 -0400

    Commit por abenamar

--- holamundo.c
+++ holamundo.c
@@ -2,7 +2,8 @@
 
 int main(){
 printf("Hola mundo\n");
-printf("%s\n", strcat(c,", soy i62chsaf"));
+printf("%s\n", strcat(c,", soy abenamar"));
+printf("En colaboración con i62chsaf")
 return 0;
 }
 
